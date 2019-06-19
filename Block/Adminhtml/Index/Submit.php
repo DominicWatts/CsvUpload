73219620962e87c0717197fcc4d0ecea48012aa2
Dominic Watts <dominic@xigen.co.uk>
2019-06-19 16:41:28 +0100

    no message

---
+++ Block/Adminhtml/Index/Submit.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Xigen\CsvUpload\Block\Adminhtml\Index;
+
+/**
+ * Submit block class
+ */
+class Submit extends \Magento\Backend\Block\Template
+{
+    /**
+     * Constructor
+     * @param \Magento\Backend\Block\Template\Context  $context
+     * @param array $data
+     */
+    public function __construct(
+        \Magento\Backend\Block\Template\Context $context,
+        array $data = []
+    ) {
+        parent::__construct($context, $data);
+    }
+}
